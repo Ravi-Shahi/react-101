3685f66510d8eee5e9b03654e9171885df2a3e17
Ravindar Kumar Shahi <ravishahi1810@gmail.com>
2023-06-04 10:50:01 +0530

    local storage

---
+++ Basics JavaScript/javascript-101/JavaScript/localStorage.js
@@ -0,0 +1,12 @@
+/*
+localStorage allows you to store key/value pair in browser.
+*/
+
+//Save Data
+localStorage.setItem("Key", "value");
+//Read Data
+localStorage.getItem("key");
+//Remove Data
+localStorage.removeItem("key");
+//Remove All
+localStorage.clear();
